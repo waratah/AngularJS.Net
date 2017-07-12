324e50c4534ca4d72547d357dc37cdbfc11f9038
unknown <kFoskey@IT-009.BETEZY.LOCAL>
2017-07-12 18:11:36 +1000

    Fix Layout issues

--- IAssetTechnical/Areas/Typescript/app.ts
+++ IAssetTechnical/Areas/Typescript/app.ts
@@ -9,13 +9,13 @@
 
         weather?: IWeatherResult,
 
-        IsLoaded: boolean,
-        Success: boolean,
-        IsAjaxBusy: number,
+        isLoaded: boolean,
+        success: boolean,
+        isAjaxBusy: number,
 
         countries: string[],
         cities: string[],
-        Errors: string[];
+        errors: string[];
     }
 }
 
@@ -23,30 +23,31 @@ angular.module('App', ['ngSanitize', 'ngAnimate', 'ui.bootstrap']).
     controller('AppController', function ($scope: IAppScope, $http: angular.IHttpService, dataService: IDataService) {
 
         $scope.IsAjax = () => {
-            return $scope.model.IsAjaxBusy != 0;
+            console.log($scope.model.isAjaxBusy);
+            return $scope.model.isAjaxBusy != 0;
         };
 
         $scope.IsLoaded = () => {
-            return $scope.model.IsLoaded;
+            return $scope.model.isLoaded;
         };
 
         $scope.GetCities = () => {
             $scope.model.cities = [];
             if (!$scope.model.country) {
-                $scope.model.Errors = ["Please enter a country name"];
+                $scope.model.errors = ["Please enter a country name"];
                 return;
             }
 
-            $scope.model.IsAjaxBusy += 1;
+            $scope.model.isAjaxBusy += 1;
             var svc = dataService.GetCities($scope.model.country);
             svc
                 .then(function (data) {
                     $scope.model.cities = data.data;
-                    $scope.model.IsAjaxBusy -= 1;
-                    $scope.model.Errors = [];
+                    $scope.model.isAjaxBusy -= 1;
+                    $scope.model.errors = [];
                 }).catch(function (reason: any) {
-                    $scope.model.Errors = [reason];
-                    $scope.model.IsAjaxBusy -= 1;
+                    $scope.model.errors = [reason];
+                    $scope.model.isAjaxBusy -= 1;
                 });
         };
 
@@ -54,54 +55,54 @@ angular.module('App', ['ngSanitize', 'ngAnimate', 'ui.bootstrap']).
             $scope.model.weather = null;
             if (!$scope.model.country) {
                 $scope.model.cities = [];
-                $scope.model.Errors = ["Please enter a country name"];
+                $scope.model.errors = ["Please enter a country name"];
                 return;
             }
             if (!$scope.model.city) {
-                $scope.model.Errors = ["Please enter a city"];
+                $scope.model.errors = ["Please enter a city"];
                 return;
             }
 
-            $scope.model.IsAjaxBusy += 1;
+            $scope.model.isAjaxBusy += 1;
             var svc = dataService.GetWeather($scope.model.country, $scope.model.city);
             svc
                 .then(function (data) {
                     $scope.model.weather= data.data;
                     console.log(data);
-                    $scope.model.IsAjaxBusy -= 1;
-                    $scope.model.Errors = [];
+                    $scope.model.isAjaxBusy -= 1;
+                    $scope.model.errors = [];
                 }).catch(function (reason: any) {
-                    $scope.model.Errors = [reason];
-                    $scope.model.IsAjaxBusy -= 1;
+                    $scope.model.errors = [reason];
+                    $scope.model.isAjaxBusy -= 1;
                 });
         };
 
         $scope.model = {
             country:"Australia",
             city: null,
-            IsLoaded: false,
-            Success: false,
-            IsAjaxBusy: 0,
-            Errors: [],
+            isLoaded: false,
+            success: false,
+            isAjaxBusy: 0,
+            errors: [],
             cities: [],
             countries: []
         };
 
         function GetCountries() {
-            $scope.model.IsAjaxBusy += 1;
+            $scope.model.isAjaxBusy += 1;
             var svc = dataService.GetCountries();
             svc
                 .then(function (data) {
-                    $scope.model.Success = true;
+                    $scope.model.success = true;
                     $scope.model.countries = data.data;
-                    $scope.model.Errors = [];
-                    $scope.model.IsLoaded = true;
-                    $scope.model.IsAjaxBusy -= 1;
+                    $scope.model.errors = [];
+                    $scope.model.isLoaded = true;
+                    $scope.model.isAjaxBusy -= 1;
                 }).catch(function (reason: any) {
-                    $scope.model.Success = false;
-                    $scope.model.Errors = [reason];
-                    $scope.model.IsLoaded = true;
-                    $scope.model.IsAjaxBusy -= 1;
+                    $scope.model.success = false;
+                    $scope.model.errors = [reason];
+                    $scope.model.isLoaded = true;
+                    $scope.model.isAjaxBusy -= 1;
                 });
         }
 
